0a6efaae0ce64198c51245d40db8beb932430ab2
harsh <harshukpandey@gmail.com>
2026-06-17 22:31:24 +0530

    edit and search functionality

--- main.go
+++ main.go
@@ -36,7 +36,9 @@ func main(){
 	fmt.Println("2. List Tasks")
 	fmt.Println("3. Complete Tasks")
 	fmt.Println("4. Delete task")
-	fmt.Println("5. Exit")
+	fmt.Println("5. Edit Task")
+	fmt.Println("6. Search Task")
+    fmt.Println("7. Exit")
 
     fmt.Print("choose: ")
     scanner.Scan()
@@ -74,17 +76,36 @@ func main(){
 		saveTasks()
 
 	case 5:
-		fmt.Println("See you later")
+		var id int
+		fmt.Print("Task ID: ")
+		scanner.Scan()
+		fmt.Sscanf(scanner.Text(), "%d", &id)
+
+		fmt.Print("New task title: ")
+		scanner.Scan()
+		newTitle := strings.TrimSpace(scanner.Text())
+
+		if editTask(id, newTitle){
+        saveTasks()
+		fmt.Println("Task Updated")
+		} else {
+			fmt.Println("Task not found")
+		}
+
+
+	case 6:
+		fmt.Println("Search Keyword: ")
+		scanner.Scan()
+		keyword:= strings.TrimSpace(scanner.Text())
+		searchTask(keyword)
+
+	case 7:
+		fmt.Println("See you Later")
 		return
 	}
   }
 }
 
-// addTask("aaj Paneer bnega")
-	// addTask("Socha to yhi hai")
-	// fmt.Println("Todo App")
-
-// var tasks []Task
 func addTask(title string){
 	task := Task{
 		// ID: len(tasks) + 1,
@@ -94,11 +115,26 @@ func addTask(title string){
 	}
 	tasks = append(tasks, task)
 }
+func editTask(id int, newTitle string) bool{
+	for i := range tasks {
+		if tasks[i].ID == id {
+			tasks[i].Title = newTitle
+			return true
+		}
+// 		if editTask(id, newTitle) {
+// 	saveTasks()
+// 	fmt.Println("Task updated")
+// } else {
+// 	fmt.Println("Task not found")
+// }
+	}
+	return false
+}
 func listTasks(){
 	for _, task := range tasks {
-		status := ""
+		status := " "
 		if task.Completed {
-			status = "-/"
+			status = "✓"
 		}
 		fmt.Printf("[%s] %d - %s\n",
 	status,
@@ -107,24 +143,34 @@ func listTasks(){
 	}
 }
 func saveTasks(){
-	data, _ := json.Marshal(tasks)
-	os.WriteFile("tasks.json", data, 0644)
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		fmt.Println("Error saving tasks:", err)
+		return
+	}
+	err = os.WriteFile("tasks.json", data,0644)
+	if err != nil {
+		fmt.Println("Error writing file:",err )
+	}
+	// os.WriteFile("tasks.json", data, 0644)
 }
-func completeTask(id int) {
+func completeTask(id int) bool {
 	for i:= range tasks {
 		if tasks[i].ID == id {
 			tasks[i].Completed = true
-			return
+			return true
 		}
 	}
+	return false
 }
-func deleteTask(id int) {
+func deleteTask(id int) bool {
 	for i, task := range tasks {
 		if task.ID == id {
 			tasks = append(tasks[:i], tasks[i+1:]...)
-		return
+			return true
 		}
 	}
+	return false
 }
 func loadTasks(){
 	data, err := os.ReadFile("tasks.json")
@@ -133,4 +179,31 @@ func loadTasks(){
 	}
 	json.Unmarshal(data, &tasks)
   }
+func searchTask(keyword string) {
+	found := false
+
+	for _, task := range tasks {
+		if strings.Contains(
+			strings.ToLower(task.Title),
+			strings.ToLower(keyword),
+		) {
+			status := " "
+			if task.Completed {
+				status = "✓"
+			}
+
+			fmt.Printf("[%s] %d - %s\n",
+				status,
+				task.ID,
+				task.Title,
+			)
+
+			found = true
+		}
+	}
+
+	if !found {
+		fmt.Println("No matching tasks found")
+	}
+}
 
